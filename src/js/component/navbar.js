0d0968251c97ede11810e68b094681eb7956f22f
XimenaAvalos <164333573+XimenaAvalos@users.noreply.github.com>
2024-08-08 18:13:09 +0000

    starwars

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import '../../styles/styles.css';
 import FavoritesDropdown from './favoritesDropdown';
 
 export const Navbar = () => {
-  const toggleDarkMode = () => {
-    document.body.classList.toggle('dark-mode');
-  };
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        // Verificar el modo inicial según la clase en el cuerpo
+        const initialMode = document.body.classList.contains('dark-mode');
+        setIsDarkMode(initialMode);
+    }, []);
+
+    const toggleDarkMode = () => {
+        document.body.classList.toggle('dark-mode');
+        setIsDarkMode(!isDarkMode);
+    };
 
   return (
     <nav className="navbar navbar-light bg-light mb-3 small-navbar">
@@ -15,8 +25,10 @@ export const Navbar = () => {
             <img src="https://static.vecteezy.com/system/resources/previews/027/127/457/non_2x/star-wars-logo-star-wars-icon-transparent-free-png.png" alt="Star Wars Logo" className="logo-img" />
           </div>
         </Link>
-        <div className="d-flex align-items-center">
-          <button onClick={toggleDarkMode} className="btn btn-dark mr-2">Dark Mode</button>
+        <div className="ml-auto d-flex align-items-center">
+                <button onClick={toggleDarkMode} className="dark-mode-toggle btn btn-secondary mr-3">
+                    {isDarkMode ? 'Light Side' : 'Dark Side'}
+                </button>
           <FavoritesDropdown />
         </div>
       </div>
