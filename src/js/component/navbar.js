853aeae56ec274380aa0674cd24cf39aa39e6872
XimenaAvalos <164333573+XimenaAvalos@users.noreply.github.com>
2024-08-08 17:58:00 +0000

    starwars

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FavoritesDropdown from './favoritesDropdown';
-import '../../styles/styles.css';
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light small-navbar">
-            <div className="container d-flex justify-content-between align-items-center">
-                <Link className="navbar-brand" to="/">
-                    <div className="logo-container">
-                        <img src="https://static.vecteezy.com/system/resources/previews/027/127/457/non_2x/star-wars-logo-star-wars-icon-transparent-free-png.png" alt="Star Wars Logo" className="logo-img" />
-                    </div>
-                </Link>
-                <div className="d-flex align-items-center dropdown-container">
-                    <FavoritesDropdown />
-                </div>
-            </div>
-        </nav>
-    );
+  const toggleDarkMode = () => {
+    document.body.classList.toggle('dark-mode');
+  };
+
+  return (
+    <nav className="navbar navbar-light bg-light mb-3 small-navbar">
+      <div className="container d-flex justify-content-between align-items-center">
+        <Link to="/">
+          <div className="logo-container">
+            <img src="https://static.vecteezy.com/system/resources/previews/027/127/457/non_2x/star-wars-logo-star-wars-icon-transparent-free-png.png" alt="Star Wars Logo" className="logo-img" />
+          </div>
+        </Link>
+        <div className="d-flex align-items-center">
+          <button onClick={toggleDarkMode} className="btn btn-dark mr-2">Dark Mode</button>
+          <FavoritesDropdown />
+        </div>
+      </div>
+    </nav>
+  );
 };
